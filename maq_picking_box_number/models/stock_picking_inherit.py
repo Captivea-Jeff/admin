20e94cdc214b8b9c07e2d879f9b6f25b50c0ba0d
unknown <pratik.bhandirge@bistasolutions.com>
2021-10-19 20:39:08 +0530

    [REF] Ticket 10108 - This is still not functioning correctly. Many important elements are missing. We need to be able to function the same way that we do in v11 with all necessary information showing and/or editable in the OPERATIONS tab as per below:
    - Vendor Code - (not editable)
    - TF Checkbox (editable)
    - Initial Demand (editable upon unlock)
    - Print Qty (editable)
    - Reserved (not editable)
    - Done (editable)
    - Unit of Measure (not editable)
    - Verified checkbox (editable)
    - Box Number (editable)

--- maq_picking_box_number/models/stock_picking_inherit.py
+++ maq_picking_box_number/models/stock_picking_inherit.py
@@ -6,6 +6,34 @@ from odoo import models, fields, api, _
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
+    @api.multi
+    @api.depends('picking_id.partner_id', 'picking_id.company_id', 'product_id')
+    def _get_vendor_code(self):
+        '''
+        This method sets the vendor code by searching vendor code from vendor pricelists on stock move. If no vendor code is set,
+        then blank value is set for the field.
+        '''
+        for rec in self:
+            vendor_name = rec.picking_id.partner_id.id
+            company_id = rec.picking_id.company_id.id
+            product_id = rec.product_id.id
+            supplierinfo_search = self.env['product.supplierinfo'].search([('name','=',vendor_name),('product_id','=',product_id),('company_id','=',company_id)], limit=1)
+            vendor_code = ''
+            if supplierinfo_search:
+                vendor_code = supplierinfo_search.product_code or ''
+            rec.m_vendor_code = vendor_code
+
     '''Added Box number field in stock.move model'''
 
     m_box_number = fields.Char(string="Box Number")
+
+    '''Added Vendor Code field in stock.move model'''
+
+    m_vendor_code = fields.Char(string="Vendor Code", compute="_get_vendor_code",
+                                help="This vendor's product code will be used when printing a request for quotation. Keep empty to use the internal one.",
+                                track_visibility="onchange")
+
+    '''Added Vendor Code field in stock.move model'''
+
+    print_qty = fields.Integer(string="Print Qty")
+
