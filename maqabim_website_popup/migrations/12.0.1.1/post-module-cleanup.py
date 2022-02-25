8f1f277535457c9fd26a2c1b1330eedf1c5b8579
Keyur Gajjar <kga@odoo.com>
2022-02-25 08:43:42 -0800

    [IMP] disable blocking views.

--- maqabim_website_popup/migrations/12.0.1.1/post-module-cleanup.py
+++ maqabim_website_popup/migrations/12.0.1.1/post-module-cleanup.py
@@ -7,7 +7,8 @@ def migrate(cr, version):
     print("Enabling multi-website...")
     # cr.execute("insert into res_groups_users_rel (uid, gid) values (2, (select id from res_groups where name = 'Multi-website'));")
     cr.execute("update ir_module_module set state = 'uninstalled' where name = 'website_multi';")
-    cr.execute("update ir_ui_view set active = 'f' where id in (select res_id from ir_model_data where module = 'website_multi');")
+    cr.execute("update ir_ui_view set active = False where id in (select res_id from ir_model_data where module = 'website_multi');")
+    cr.execute("update ir_ui_view set active = False where id in (1990,1543);")
 
     print("Moving registered_on_website_id to website_id on the res.partner...")
     cr.execute("UPDATE res_partner SET website_id = res_users.registered_on_website_id FROM res_users WHERE res_partner.user_id = res_users.id;")
