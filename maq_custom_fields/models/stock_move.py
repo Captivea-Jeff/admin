37d3842d3f73856bf596f77c3bcce372fe44d1bc
Dhaval Panchal <dhaval.panchal@konsultoo.com>
2022-08-10 10:21:05 +0530

    [ADD] maq_custom_fields: Add Migrated module of V15

--- maq_custom_fields/models/stock_move.py
+++ maq_custom_fields/models/stock_move.py
@@ -4,10 +4,12 @@ from odoo import models, fields, api
 
 
 class StockMove(models.Model):
-
     _inherit = 'stock.move'
     
-    @api.multi
+    m_vendor_code = fields.Char(string="Vendor Code", compute="_get_vendor_code",
+                                help="This vendor's product code will be used when printing a request for quotation. Keep empty to use the internal one.",
+                                tracking=True)
+
     @api.depends('picking_id.partner_id', 'picking_id.company_id', 'product_id')
     def _get_vendor_code(self):
         '''
@@ -15,15 +17,8 @@ class StockMove(models.Model):
         then blank value is set for the field.
         '''
         for rec in self:
-            vendor_name = rec.picking_id.partner_id.id
-            company_id = rec.picking_id.company_id.id
-            product_id = rec.product_id.id
-            supplierinfo_search = self.env['product.supplierinfo'].search([('name','=',vendor_name),('product_id','=',product_id),('company_id','=',company_id)], limit=1)
+            supplierinfo_search = self.env['product.supplierinfo'].search([('name','=',rec.picking_id.partner_id.id),('product_id','=',rec.product_id.id),('company_id','=',rec.picking_id.company_id.id)], limit=1)
             vendor_code = ''
             if supplierinfo_search:
                 vendor_code = supplierinfo_search.product_code or ''
             rec.m_vendor_code = vendor_code
-
-    m_vendor_code = fields.Char(string="Vendor Code", compute="_get_vendor_code",
-                                help="This vendor's product code will be used when printing a request for quotation. Keep empty to use the internal one.",
-                                track_visibility="onchange")
