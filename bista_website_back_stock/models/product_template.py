84a5c22efa73dbcf7a35537665f459407f3a6b02
Joel Tovar <96855747+Captivea-JTO@users.noreply.github.com>
2022-08-29 09:54:23 -0400

    clear branch

--- bista_website_back_stock/models/product_template.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-from odoo import fields, models
-
-class ProductTemplate(models.Model):
-    _inherit='product.template'
-
-    bck_stock_date = fields.Datetime('Back In Stock Date', company_dependent=True)
--- ecommerce_category/models/res_config_settings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    website_shop_login = fields.Boolean(related='website_id.website_shop_login')
-    website_shop_login_redirect = fields.Char(related='website_id.website_shop_login_redirect')
