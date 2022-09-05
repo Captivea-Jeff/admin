4862f29c5bc949ebe4a18fe2909aee4b85411b22
Joel Tovar <96855747+Captivea-JTO@users.noreply.github.com>
2022-09-05 13:32:08 -0400

    [ADD] bista_product_pricelist

---
+++ bista_product_pricelist/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from . import models
+
+from odoo import api, SUPERUSER_ID
+
+_logger = logging.getLogger(__name__)
+
+def _update_sales_pricelist(cr, registry):
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    products = env['product.template'].search([('sale_ok','=', True)])
+
+    for product in products:
+        product_vals = {}
+        product_name = str(product.name) or ''
+
+        ppis = env['product.pricelist.item'].search([('product_tmpl_id','=',product.id)])
+
+        sales_pricelist = False
+        if ppis:
+            for ppi in ppis:
+                if ppi.min_quantity in [0,1] and ppi.date_start == False and ppi.date_end == False:
+
+                    sales_pricelist = ppi.fixed_price
+
+            product_vals.update({'sales_pricelist':sales_pricelist})
+            print ("product_vals --->>", product_vals)
+
+        if product_vals:
+            try:
+                product.write(product_vals)
+            except Exception as e:
+                _logger.error(
+                    'product name - ' + product_name + ' : %s', e)
+                pass
