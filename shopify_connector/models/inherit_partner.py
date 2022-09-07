1c46bc9362f47447927031b40f33fec3abb775bf
Joel Tovar <96855747+Captivea-JTO@users.noreply.github.com>
2022-09-07 13:56:09 -0400

    [ADD] shopify_connector maqabim_purchase

---
+++ shopify_connector/models/inherit_partner.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    is_shopify = fields.Boolean(string="Is Shopify",
+                                help="Consider this record while importing SO from Shopify",
+                                tracking=True)
